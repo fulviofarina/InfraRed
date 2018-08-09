b8b414c9beb309a86f057a891a5059838396578e
Fulvio Farina <fulviofarina@gmail.com>
2018-08-08 22:17:19 -0400

    v1

--- InfraRed/InfraRed.ino
+++ InfraRed/InfraRed.ino
@@ -8,18 +8,13 @@
 #include "Msg.h"
 #include "Tools.h"
 #include "SerialEvent.h"
-
 #include "InfraRedData.h"
-
-
 
 // the setup function runs once when you press reset or power the board
 void setup() {
 
 	Serial.begin(153600);
-
 	setupSerialEvent();
-
 	InfraRedData.configurePins(2, 14U, 9U, 100);
 	InfraRedData.begin(true);
 }
@@ -28,15 +23,11 @@ void setup() {
 void loop() {
 
 	InfraRedData.listen();
-
 	String msg = loopSerialEvent();
-
 	if (msg.compareTo("") != 0)
 	{
 		InfraRedData.sendMsg(msg.c_str(), 1U);
 	}
-
-
 }
 
 
