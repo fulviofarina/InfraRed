0b86fb7bc2561e81031c9e3d611adc9341866c6f
Fulvio Farina <fulviofarina@gmail.com>
2018-08-08 20:02:19 -0400

    Add project files.

---
+++ InfraRed/InfraRed.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		InfraRed.ino
+ Created:	8/8/2018 20:02:18
+ Author:	Fulvio
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
